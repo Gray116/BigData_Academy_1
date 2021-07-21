467bb7b2279e70fc69cdc784ac6da6cddd12fd7d
Gray116 <swkim116@naver.com>
2021-07-21 18:13:21 +0900

    0721

--- 07_Python/IDEtest/pycharm.py
+++ 07_Python/IDEtest/pycharm.py
@@ -7,3 +7,10 @@ print(a_num)
 a = 'one'
 a_num = 1
 print(type(a_num))
+
+web = "abc"
+web_n = 3
+web_list = [ "ab" , "cd" , "de" ," gd"]
+
+print(web)
+
---
+++ 07_Python/a.py
@@ -0,0 +1,6 @@
+def test():
+    print("Hello, World")
+    print("더운날 건강조심하세요")
+if __name__ == '__main__':
+    test()
+    print(__name__)
